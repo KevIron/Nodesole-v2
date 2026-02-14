f74f1ff074ce6221d25c3dff78f3be146d1dd602
Marcel Kalużny <marcelkaluzny123@gmail.com>
2026-02-15 00:54:35 +0100

    Fix node context function name

--- src/hooks/useNodeContext.tsx
+++ src/hooks/useNodeContext.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { NodeContext } from "../contexts/NodeContext";
 
-export default function useViewportContext() {
+export default function useNodeContext() {
   const nodeContext = useContext(NodeContext);
   if (!nodeContext) throw new Error("Not inside of an node context!");
 
