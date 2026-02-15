cc18d0ede5d13cbf5479b43ed127e1c70b50ce7d
Marcel Kalużny <marcelkaluzny123@gmail.com>
2026-02-15 21:39:15 +0100

    Remove debug side panel state display

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -3,8 +3,7 @@ import Vec2 from "../utils/Vec2";
 
 export default function SidePanel() {
   const addNode = useEditorStore((state) => state.addNode);
-  const connections = useEditorStore((state) => state);
-
+  
   function handleAddEntryNode() {
     addNode({
       id: crypto.randomUUID(),
@@ -30,10 +29,6 @@ export default function SidePanel() {
       <h2>Development options:</h2>
       <button onClick={handleAddEndNode}>Add end node</button>
       <button onClick={handleAddEntryNode}>Add entry node</button>
-
-      <textarea value={JSON.stringify(connections)} readOnly>
-
-      </textarea>
     </aside>
   );
 }
