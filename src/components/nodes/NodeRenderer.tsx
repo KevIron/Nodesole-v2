62f490cbcad8eb702e2e16c441766e2573d46306
Marcel Kalużny <marcelkaluzny123@gmail.com>
2026-03-03 22:18:06 +0100

    Remove duplicate factory class

--- src/components/nodes/NodeRenderer.tsx
+++ src/components/nodes/NodeRenderer.tsx
@@ -1,7 +1,11 @@
-import EndNode from "./nodeTypes/EndNode";
-import EntryNode from "./nodeTypes/EntryNode";
 import { memo } from "react";
 import { useEditorStore } from "../../store/editorStore";
+
+import EndNode from "./nodeTypes/EndNode";
+import EntryNode from "./nodeTypes/EntryNode";
+import ConditionNode from "./nodeTypes/ConditionNode";
+import ConstantEmitterNode from "./nodeTypes/ConstantEmitterNode";
+import OperatorNode from "./nodeTypes/OperatorNode";
 
 import type { NodeProps, NodeTypes } from "../../types/EditorTypes";
 
@@ -12,6 +16,9 @@ type NodeRendererProps = {
 const NODE_COMPONENT_MAP = {
   ENTRY_NODE: EntryNode,
   END_NODE: EndNode,
+  CONDITION_NODE: ConditionNode,
+  CONSTANT_EMITTER_NODE: ConstantEmitterNode,
+  OPERATOR_NODE: OperatorNode
 } satisfies {
   [K in NodeTypes]: React.ComponentType<NodeProps>;
 };
